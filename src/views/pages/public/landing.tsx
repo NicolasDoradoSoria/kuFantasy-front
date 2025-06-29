5de62f0f4ee2e39373993816867a9a2b2daa893d
kirinico <kirinico@gmail.com>
2025-06-29 11:31:02 +0900

    se creo la pantalla de login

--- src/views/pages/public/landing.tsx
+++ src/views/pages/public/landing.tsx
@@ -5,34 +5,38 @@ const LandingPage = () => {
     <div className="relative h-screen w-full bg-[url('/fondo.jpg')] bg-cover bg-center">
       
       <div className="absolute inset-0 bg-black/60 z-0"></div>
-      <img src="/enano.png" className="absolute bottom-0 right-0 w-32 opacity-80 z-10 drop-shadow-lg" />
+      
+      <img src="/elfo.png" className="absolute bottom-0 left-0 w-32 opacity-30 z-0" />
+      <img src="/enano1.png" className="absolute bottom-0 right-0 w-32 opacity-80 z-10 drop-shadow-lg" />
+
       <p className="text-xl italic text-gray-300 mb-4">Forja tu destino en las tierras de Kufantasy</p>
 
-        <motion.div
-            initial={{ opacity: 0, y: -50 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 2 }}
-            className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4">
-              <h1 className="text-4xl md:text-5xl font-extrabold mb-6 drop-shadow-xl">
-                Regístrate para jugar
-              </h1>
-          <motion.button
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}
-                className="bg-red-600 hover:bg-red-700 transition-colors duration-300 px-8 py-3 rounded-xl text-lg font-semibold shadow-lg">
-                Jugar
-          </motion.button>
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2 }}
+        className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white px-4">
+          <h1 className="text-4xl md:text-5xl font-extrabold mb-6 drop-shadow-xl">
+            Regístrate para jugar
+          </h1>
 
-          <motion.p
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 1.3, duration: 0.8 }}
-            className="mt-6 text-base md:text-lg text-red-200 max-w-xl drop-shadow"
-          >
-            Un mundo medieval, elige a tu PJ, completa misiones, comercia e interactúa.
-          </motion.p>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          className="bg-red-600 hover:bg-red-700 transition-colors duration-300 px-8 py-3 rounded-xl text-lg font-semibold shadow-lg">
+          Jugar
+        </motion.button>
+
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1.3, duration: 0.8 }}
+          className="mt-6 text-base md:text-lg text-red-200 max-w-xl drop-shadow"
+        >
+          Un mundo medieval, elige a tu PJ, completa misiones, comercia e interactúa.
+        </motion.p>
           
-        </motion.div>
+      </motion.div>
 
     </div> );
 }
--- src/views/pages/public/login.tsx
+++ src/views/pages/public/login.tsx
@@ -1,5 +1,62 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { FaEye, FaEyeSlash  } from "react-icons/fa";
+
 const LoginPage = () => {
-    return <h1>login page</h1>
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+        <div className="h-screen flex">
+            <motion.div 
+                className="w-full md:w-1/2 bg-gray-200 flex flex-col justify-center items-center p-6"
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8 }}>
+                    <h1 className="text-3xl font-extrabold mb-6 text-black text-center">
+                        Bienvenido a Ku-Fantasy
+                    </h1>
+
+                    <form className="w-full max-w-sm">
+
+                        <motion.div
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.6, delay: 0.2 }}
+                        className="mb-4">
+                            <label htmlFor="email" className="block text-black text-sm font-medium mb-2">Email</label>
+                            <input type="email" className="w-full px-4 py-2 rounded-md bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500" />
+                        </motion.div>
+
+                        <motion.div
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.6, delay: 0.4 }}
+                        className="mb-6 relative">
+                            <label htmlFor="password" className="block text-black text-sm font-medium mb-2">Contraseña</label>
+                            <input type={showPassword ? "text" : "password"} className="w-full px-4 py-2 rounded-md bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500" />
+                            <span
+                                onClick={() => setShowPassword((prev) => !prev)}
+                                className="absolute right-3 top-10 cursor-pointer text-gray-500 hover:text-gray-800">
+                                {showPassword ? <FaEyeSlash /> : <FaEye />}
+                            </span>
+                        </motion.div>
+
+                        <motion.button
+                            whileHover={{ scale: 1.05, boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.5)" }}
+                            whileTap={{ scale: 0.95 }}
+                            type="submit"
+                            className="w-full bg-black text-white font-semibold py-2 rounded-md transition cursor-pointer"
+                        >
+                            Iniciar sesión
+                        </motion.button>
+                    </form>
+            </motion.div>
+
+            <div className="hidden md:flex w-1/2">
+                <img src="/login-2.jpeg" alt="Login Background" className="w-full h-full object-cover" />
+            </div>
+        </div>
+    )
 }
  
 export default LoginPage;
